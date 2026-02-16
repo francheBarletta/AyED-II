e11c594741331b941e9c7f710aafe9c7bad90ef0
Francesco Antonio Barletta <franchebarletta@gmail.com>
2026-02-16 17:31:10 -0300

    pos_neg_counter

---
+++ review/pos_neg_counter.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int pos = 0;
+    int neg = 0;
+    int ceros = 0;
+    int i = 0;
+
+    while (i < 10)
+    {
+        printf("Ingrese un numero Real: ");
+        scanf("%d", &x);
+        if (x > 0)
+        {
+            pos = pos + 1;
+        }
+        else if (x < 0)
+        {
+            neg = neg + 1;
+        }
+        else
+        {
+            ceros = ceros + 1;
+        }
+        i++;
+    }
+    printf("La cantidad de positivos es la siguiente: %d \n", pos);
+    printf("La cantidad de negativos es la siguiente: %d \n", neg);
+    printf("La cantidad de ceros es la siguiente: %d \n", ceros);
+
+    return 0;
+}
+
+/*🟠 Ejercicio 2.2 — Contador con condición
+
+Pedí 10 números al usuario.
+
+Mostrá:
+
+Cuántos son positivos
+
+Cuántos son negativos
+
+Cuántos son cero*/
