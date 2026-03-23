eb91b88d80a104e0a092218956a66877c857c1b7
Francesco Antonio Barletta <franchebarletta@gmail.com>
2026-03-23 19:31:34 -0300

    bound update

--- now/bound.c
+++ now/bound.c
@@ -4,53 +4,59 @@
 
 #define ARRAY_SIZE 4
 
-struct bound_data {
-    bool is_upperbound;
-    bool is_lowerbound;
-    bool exists;
-    unsigned int where;
+struct bound_data
+{
+	bool is_upperbound;
+	bool is_lowerbound;
+	bool exists;
+	unsigned int where;
 };
 
-struct bound_data check_bound(int value, int arr[], unsigned int length) {
-    struct bound_data res = {true, true, false, 0};
-    	int i = 0;
-    	
-    	while(i < length){
-    		if(arr[i] > value){
-    			res.is_upperbound = false;
-    		}
-    		if(arr[i] < value){
-    			res.is_lowerbound = false;
-    		} 
-    		if(!res.exists && arr[i] == value){
-    			res.exists = true;
-    			res.where = i;
-    		}
-    		i++;
-    	}
-    return res;
+struct bound_data check_bound(int value, int arr[], unsigned int length)
+{
+	struct bound_data res = {true, true, false, 0};
+	int i = 0;
+
+	while (i < length)
+	{
+		if (arr[i] > value)
+		{
+			res.is_upperbound = false;
+		}
+		if (arr[i] < value)
+		{
+			res.is_lowerbound = false;
+		}
+		if (!res.exists && arr[i] == value){
+			 res.exists = true;
+			 res.where = i;
+		}
+		i++;
+	}
+	return res;
 }
 
-int main(void) {
-    int a[ARRAY_SIZE];
-    int value=9;
-    
-    int i = 0;
-    	printf("Ingrese un Numero entero: \n");
-    	scanf("%d", &value);
-	
-    while(i < ARRAY_SIZE){    
-	printf("Ingrese un valor para la posicion %d\n", i);
-	scanf("%d", &a[i]);
-	i++;
-    }
-    struct bound_data result = check_bound(value, a, ARRAY_SIZE);
+int main(void)
+{
+	int a[ARRAY_SIZE];
+	int value = 9;
 
-    printf("%d", result.is_upperbound);
-    printf("%d", result.is_lowerbound);
-    printf("%u", result.exists);
-    printf("%u", result.where);
+	int i = 0;
+	printf("Ingrese un Numero entero: \n");
+	scanf("%d", &value);
 
-    return 0;
+	while (i < ARRAY_SIZE)
+	{
+		printf("Ingrese un valor para la posicion %d\n", i);
+		scanf("%d", &a[i]);
+		i++;
+	}
+	struct bound_data result = check_bound(value, a, ARRAY_SIZE);
+
+	printf("%d", result.is_upperbound);
+	printf("%d", result.is_lowerbound);
+	printf("%u", result.exists);
+	printf("%u", result.where);
+
+	return 0;
 }
-
