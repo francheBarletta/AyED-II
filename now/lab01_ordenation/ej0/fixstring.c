bf1dfc7148f0e4972aa9c1e11ffacd20234bf39f
Francesco Antonio Barletta <franchebarletta@gmail.com>
2026-03-29 16:42:25 -0300

    ej0

---
+++ now/lab01_ordenation/ej0/fixstring.c
@@ -0,0 +1,49 @@
+#include <stdbool.h>
+#include <assert.h>
+
+#include "fixstring.h"
+
+unsigned int fstring_length(fixstring s)
+{
+    unsigned int contar = 0;
+    while (s[contar] != '\0')
+    {
+        contar++;
+    }
+    return contar;
+}
+
+bool fstring_eq(fixstring s1, fixstring s2)
+{
+    int i = 0;
+    while (s1[i] == s2[i])
+    {
+        if (s1[i] == '\0')
+        {
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
+
+bool fstring_less_eq(fixstring s1, fixstring s2)
+{
+    int i = 0;
+    while (s1[i] != '\0' && s2[i] != '\0')
+    {
+        if (s1[i] < s2[i])
+        {
+            return true;
+        }
+        if (s1[i] > s2[i])
+        {
+            return false;
+        }
+        i++;
+    }
+    return false;
+}
+/*
+terminar fstring_less_eq
+*/
