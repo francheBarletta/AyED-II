06ad701825ba481225bcaa31b88bb203be273e3a
Francesco Antonio Barletta <franchebarletta@gmail.com>
2026-03-29 16:34:52 -0300

    fixstring

--- now/lab1_ordenation/ej0/fixstring.c
+++ now/lab1_ordenation/ej0/fixstring.c
@@ -3,18 +3,23 @@
 
 #include "fixstring.h"
 
-unsigned int fstring_length(fixstring s) {
+unsigned int fstring_length(fixstring s)
+{
     unsigned int contar = 0;
-    while(s[contar] != '\0'){ 
+    while (s[contar] != '\0')
+    {
         contar++;
     }
     return contar;
 }
 
-bool fstring_eq(fixstring s1, fixstring s2) {
+bool fstring_eq(fixstring s1, fixstring s2)
+{
     int i = 0;
-    while(s1[i] == s2[i]){
-        if (s1[i] == '\0'){
+    while (s1[i] == s2[i])
+    {
+        if (s1[i] == '\0')
+        {
             return true;
         }
         i++;
@@ -22,14 +27,17 @@ bool fstring_eq(fixstring s1, fixstring s2) {
     return false;
 }
 
-
-bool fstring_less_eq(fixstring s1, fixstring s2) {
+bool fstring_less_eq(fixstring s1, fixstring s2)
+{
     int i = 0;
-    while(s1[i] != '\0' && s2[i] != '\0'){
-        if(s1[i] < s2[i]){
+    while (s1[i] != '\0' && s2[i] != '\0')
+    {
+        if (s1[i] < s2[i])
+        {
             return true;
         }
-        if(s1[i] > s2[i]){
+        if (s1[i] > s2[i])
+        {
             return false;
         }
         i++;
@@ -39,4 +47,3 @@ bool fstring_less_eq(fixstring s1, fixstring s2) {
 /*
 terminar fstring_less_eq
 */
-
--- now/lab1_ordenation/ej0/fixstring.h
+++ now/lab1_ordenation/ej0/fixstring.h
@@ -5,11 +5,10 @@
 
 #define FIXSTRING_MAX 100
 
-typedef char fixstring [FIXSTRING_MAX];
+typedef char fixstring[FIXSTRING_MAX];
 
 unsigned int fstring_length(fixstring s);
 bool fstring_eq(fixstring s1, fixstring s2);
 bool fstring_less_eq(fixstring s1, fixstring s2);
 
 #endif
-
