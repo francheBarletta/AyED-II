68506fa3ac660673ce2d11d73e7d3e5f7dd8ac51
Franche <franchebarletta@gmail.com>
2026-03-31 15:29:23 -0300

    fixstring

--- now/lab01_operation/ej0/fixstring.c
+++ now/lab01_operation/ej0/fixstring.c
@@ -3,24 +3,42 @@
 
 #include "fixstring.h"
 
-unsigned int fstring_length(fixstring s) {
-    /*
-     * COMPLETAR
-     *
-     */
+unsigned int fstring_length(fixstring s)
+{
+    unsigned int i = 0;
+    while (s[i] != '\0')
+    {
+        i++;
+    }
+    return i;
 }
 
-bool fstring_eq(fixstring s1, fixstring s2) {
-    /*
-     * COMPLETAR
-     *
-     */
+bool fstring_eq(fixstring s1, fixstring s2)
+{
+
+    unsigned int i = 0;
+    while (s1[i] == s2[i])
+    {
+        if (s1[i] == '\0')
+        {
+            return true;
+        }
+        i++;
+    }
+    return false;
 }
 
-bool fstring_less_eq(fixstring s1, fixstring s2) {
-    /*
-     * COMPLETAR
-     *
-     */
-}
+bool fstring_less_eq(fixstring s1, fixstring s2)
+{
 
+    unsigned int i = 0;
+    while (s1[i] == s2[i])
+    {
+        if (s1[i] == '\0')
+        {
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
