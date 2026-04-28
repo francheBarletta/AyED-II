a929eca8d8cc730ba98fb500bc5c0d259ea25f0c
Francesco Antonio Barletta <franchebarletta@gmail.com>
2026-04-28 11:58:43 -0300

    Quick_sort 2 :P

--- now/lab01_operation/ej3/sort.c
+++ now/lab01_operation/ej3/sort.c
@@ -7,26 +7,51 @@
 #include "sort_helpers.h"
 #include "sort.h"
 
+static unsigned int partition(int a[], unsigned int izq, unsigned int der)
+{
+    unsigned int i = izq + 1;
+    unsigned int j = der;
+    unsigned int ppiv = izq;
 
-static unsigned int partition(int a[], unsigned int izq, unsigned int der) {
-    /* Needs implementation */
+    while (i <= j)
+    {
+        if (a[i] <= a[izq])
+        {
+            i++;
+        }
+        else if (a[j] >= a[izq])
+        {
+            j--;
+        }
+        else if (a[i] > a[izq] && a[j] < a[izq])
+        {
+            swap(a, i, j);
+        }
+    }
+    swap(a, ppiv, j);
+    ppiv = j;
 
-    /* PRECONDITION: 
-       0 <= izq < der < length of the array
-
-     Permutes elements of a[izq..der] and returns pivot such that:
-     - izq <= pivot <= der
-     - elements in a[izq,pivot) all 'go_before' (according to function goes_before) a[pivot]
-     - a[pivot] 'goes_before' all the elements in a(pivot,der]
-    */
+    return j;
 }
 
-static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
-
+static void quick_sort_rec(int a[], unsigned int izq, unsigned int der)
+{
+    unsigned int ppiv;
+    if (der > izq)
+    {
+        ppiv = partition(a, izq, der);
+        quick_sort_rec(a, izq, ppiv - 1);
+        quick_sort_rec(a, ppiv + 1, der);
+    }
 }
 
-void quick_sort(int a[], unsigned int length) {
+void quick_sort(int a[], unsigned int length)
+{
     quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
 }
 
+/*
+gcc -Wall -Werror -Wextra -pedantic -std=c99 -c array_helpers.c sort.c main.c
+gcc -Wall -Werror -Wextra -pedantic -std=c99 -no-pie array_helpers.o sort.o sort_helpers.o main.o -o sorter
+./sorter input/example-unsorted.in
+*/
