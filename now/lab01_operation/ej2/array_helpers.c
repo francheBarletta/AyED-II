484f5762651f995e5561e98a0a3bce3f7de78414
Francesco Antonio Barletta <franchebarletta@gmail.com>
2026-04-28 10:16:10 -0300

    quick_sort

--- now/lab01_operation/ej2/array_helpers.c
+++ now/lab01_operation/ej2/array_helpers.c
@@ -3,39 +3,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void array_copy(int copy[], int array[], unsigned int length) {
-    for (unsigned int i = 0u; i < length; ++i) {
+void array_copy(int copy[], int array[], unsigned int length)
+{
+    for (unsigned int i = 0u; i < length; ++i)
+    {
         copy[i] = array[i];
     }
 }
 
-unsigned int array_value_count(int a[], unsigned int length, int value) {
+unsigned int array_value_count(int a[], unsigned int length, int value)
+{
     unsigned int count = 0u;
-    for (unsigned int i = 0u; i < length; ++i) {
-        if (a[i] == value) {
+    for (unsigned int i = 0u; i < length; ++i)
+    {
+        if (a[i] == value)
+        {
             ++count;
         }
     }
     return count;
 }
 
-void array_dump(int a[], unsigned int length) {
+void array_dump(int a[], unsigned int length)
+{
     fprintf(stdout, "%u\n", length);
-    for (unsigned int i = 0u; i < length; ++i) {
+    for (unsigned int i = 0u; i < length; ++i)
+    {
         fprintf(stdout, "%i", a[i]);
-        if (i < length - 1) {
+        if (i < length - 1)
+        {
             fprintf(stdout, " ");
-        } else {
+        }
+        else
+        {
             fprintf(stdout, "\n");
         }
     }
 }
 
-
-bool array_is_permutation_of(int a[], int b[], unsigned int length) {
+bool array_is_permutation_of(int a[], int b[], unsigned int length)
+{
     unsigned int i = 0u;
     bool result = true;
-    while (i < length && result) {
+    while (i < length && result)
+    {
         unsigned int a_count = array_value_count(a, length, a[i]);
         unsigned int b_count = array_value_count(b, length, a[i]);
         result = (a_count == b_count);
@@ -46,10 +57,12 @@ bool array_is_permutation_of(int a[], int b[], unsigned int length) {
 
 unsigned int array_from_file(int array[],
                              unsigned int max_size,
-                             const char *filepath) {
+                             const char *filepath)
+{
     FILE *file = NULL;
     file = fopen(filepath, "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         fprintf(stderr, "File does not exist.\n");
         exit(EXIT_FAILURE);
     }
@@ -57,17 +70,21 @@ unsigned int array_from_file(int array[],
     unsigned int size = 0u;
     int res = 0;
     res = fscanf(file, " %u ", &size);
-    if (res != 1) {
+    if (res != 1)
+    {
         fprintf(stderr, "Invalid array.\n");
         exit(EXIT_FAILURE);
     }
-    if (size > max_size) {
+    if (size > max_size)
+    {
         fprintf(stderr, "Allowed size is %d.\n", max_size);
         exit(EXIT_FAILURE);
     }
-    while (i < size) {
-        res = fscanf(file," %d ", &(array[i]));
-        if (res != 1) {
+    while (i < size)
+    {
+        res = fscanf(file, " %d ", &(array[i]));
+        if (res != 1)
+        {
             fprintf(stderr, "Invalid array.\n");
             exit(EXIT_FAILURE);
         }
--- now/lab01_operation/ej2/sort.c
+++ now/lab01_operation/ej2/sort.c
@@ -6,17 +6,53 @@
 #include "sort_helpers.h"
 #include "sort.h"
 
+/*
+static void partition(int a[], unsigned int izq, unsigned int der, unsigned int *ppiv)
+{
+    unsigned int i = izq + 1;
+    unsigned int j = der;
+    *ppiv = izq;
 
-static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
- /* needs implementation */
-
-    /* no implementes partition, ya está implementado en sort_helpers.o
-       (no se puede leer, pero en sort_helpers.h vas a encontrar información
-        para saber cómo usarlo)
-    */
+    while (i <= j)
+    {
+        if (a[i] <= a[izq])
+        {
+            i++;
+        }
+        else if (a[j] >= a[izq])
+        {
+            j--;
+        }
+        else if (a[i] > a[izq] && a[j] < a[izq])
+        {
+            swap(a, i, j);
+        }
+        swap(a, izq, j);
+        *ppiv = j;
+    }
+}
+*/
+static void quick_sort_rec(int a[], unsigned int izq, unsigned int der)
+{
+    unsigned int ppiv;
+    if (der > izq)
+    {
+        ppiv = partition(a, izq, der);
+        quick_sort_rec(a, izq, ppiv - 1);
+        quick_sort_rec(a, ppiv + 1, der);
+    }
 }
 
-void quick_sort(int a[], unsigned int length) {
+void quick_sort(int a[], unsigned int length)
+{
     quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
 }
 
+/*
+gcc -Wall -Werror -Wextra -pedantic -std=c99 -c array_helpers.c sort.c main.c
+gcc -Wall -Werror -Wextra -pedantic -std=c99 -no-pie array_helpers.o sort.o sort_helpers.o main.o -o sorter
+./sorter input/example-unsorted.in
+
+7
+1 2 3 3 5 7 9
+*/
